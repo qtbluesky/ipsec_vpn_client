a7b208e39a04791e57d7943c080d61e48bdfe033
qtbluesky <qtijay@126.com>
2020-10-13 16:54:48 +0800

    update note

--- start.sh
+++ start.sh
@@ -15,10 +15,10 @@ echo "c myvpn" > /var/run/xl2tpd/l2tp-control
 sleep 5
 echo "Configuring IP routing talbe..."
 
-# 添加本地IP 和 本地网关IP
+# 替换为本地IP 和 本地网关IP
 route add 0.0.0.0 gw 0.0.0.0
 
-# 添加vpn服务器IP 和 本地网关IP
+# 替换为vpn服务器IP 和 本地网关IP
 route add 0.0.0.0 gw 0.0.0.0
 
 #route del -net 0.0.0.0 netmask 255.255.255.255 dev eth0
